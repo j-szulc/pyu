f0c21917a6de3e8f828cf34a50fc0578996a8589
Jakub Szulc <58491724+j-szulc@users.noreply.github.com>
2024-07-03 12:17:33 +0200

    Add simple cache

--- src/pyu/functools.py
+++ src/pyu/functools.py
@@ -1,1 +1,10 @@
-# 
+
+def cache_once(filename, fun):
+    from pathlib import Path
+    from dill import dump, load
+    if Path(filename).exists():
+        with open(filename, "rb") as f:
+            return load(f)
+    result = fun()
+    with open(filename, "wb") as f:
+        dump(result, f)
